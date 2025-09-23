86ba9a26c94c19f2f6c66e1099d797b82cae8da0
Faith Etornam <faithgbadegbe1@gmail.com>
2025-09-23 00:06:53 +0000

    About finishing the rock paper scissors game

---
+++ rock_paper_scissor.py
@@ -0,0 +1,31 @@
+import random
+
+items = ('r', 'p', 's')
+
+emojis = {
+    'r': '🪨',
+    'p': '📃',
+    's': '✂️'
+}
+
+user_select = input('Rock, paper, or scissors? (r/p/s): ').lower()
+
+if user_select not in items:
+    print('Invalid choice!')
+
+random_number = random.randint(0,2)
+computer_choice = items[random_number]
+
+print(f"You chose {emojis[user_select]}")
+print(f"Computer chose {emojis[computer_choice]}")
+
+if user_select == computer_choice:
+    print('It is a tie!')
+
+elif (user_select == 'r' and computer_choice == 's') or \
+    (user_select == 'p' and computer_choice == 'r') or \
+    (user_select == 's' and computer_choice == 'p'):
+    print('You win!')
+    
+else: 
+    print('You lose!')
