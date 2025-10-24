78bd4f9bd73345ca7a850216e44db5ebaa2b744e
Faith Etornam <faithgbadegbe1@gmail.com>
2025-10-24 00:16:09 +0000

    Finished building a jpg to png converter which takes directory names as arguements

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -8,4 +8,9 @@ output_directory = sys.argv[2]
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-for filename in os.
+print(os.listdir(image_folder))
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f"{image_folder}{filename}")
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_directory}{clean_name}.png', 'png')
