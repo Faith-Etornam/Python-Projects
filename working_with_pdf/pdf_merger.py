5dca1be48b0c5e165db24b82ab633ba31314b312
Faith Etornam <faithgbadegbe1@gmail.com>
2025-10-25 00:22:56 +0000

    Making watermarks in my pdfs

--- working_with_pdf/pdf_merger.py
+++ working_with_pdf/pdf_merger.py
@@ -1,13 +1,30 @@
 import pypdf
-import sys
 
-inputs = sys.argv[1:]
 
-def pdf_merger(pdf_list):
-    merger = pypdf.PdfWriter()
-    for pdf in pdf_list:
-        merger.append(pdf)
-    merger.write('merged.pdf')
-    merger.close()
+template = pypdf.PdfReader(open('merged.pdf', 'rb'))
 
-pdf_merger(inputs)
+watermark = pypdf.PdfReader(open('wtr.pdf', 'rb'))
+
+output = pypdf.PdfWriter()
+
+for i in range(len(template.pages)):
+    page = template.pages[0]
+    page.merge_page(watermark.pages[0])
+    output.add_page(page)
+
+    with open('watermarked.pdf', 'wb') as new_pdf_file:
+        output.write(new_pdf_file)
+
+
+
+
+# inputs = sys.argv[1:]
+
+# def pdf_merger(pdf_list):
+#     merger = pypdf.PdfWriter()
+#     for pdf in pdf_list:
+#         merger.append(pdf)
+#     merger.write('merged.pdf')
+#     merger.close()
+
+# pdf_merger(inputs)
